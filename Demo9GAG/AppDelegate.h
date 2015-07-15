a4326b810d9ddffde8566493b2c92c77f38704cd
Vince Yuan <vince.yuan@gmail.com>
2015-07-15 17:25:31 +0800

    Initial Commit

---
+++ Demo9GAG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo9GAG
+//
+//  Created by Vince Yuan on 15/7/15.
+//  Copyright © 2015 Vince Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo9GAG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo9GAG
+//
+//  Created by Vince Yuan on 15/7/15.
+//  Copyright © 2015 Vince Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
